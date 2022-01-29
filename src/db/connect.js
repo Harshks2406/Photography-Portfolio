8dd3087c79d8a7ca12bf5583df1687f9c652f544
Harshks2406 <harshsingh.hks@gmail.com>
2022-01-30 00:20:46 +0530

    Some Changes

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/photography",{  
+mongoose.connect(process.env.MONGODB_URL ||"mongodb://localhost:27017/photography",{  
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 
